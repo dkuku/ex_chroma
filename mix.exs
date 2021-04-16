d10adbef74cd6a88661bdc0cbe5a301cf75c237c
Daniel Kukula <daniel.kuku@gmail.com>
2021-04-16 19:20:06 +0100

    add git_ops

--- mix.exs
+++ mix.exs
@@ -1,23 +1,36 @@
 defmodule ExChroma.MixProject do
   use Mix.Project
 
+  @version "0.1.0" 
+  @source_url "https://github.com/dkuku/ex_chroma"
+
   def project do
     [
       app: :ex_chroma,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      docs: docs(),
       description: description(),
       package: package(),
       deps: deps(),
       name: "ex_chroma",
-      source_url: "https://github.com/dkuku/ex_chroma"
+      source_url: @source_url,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
   def application do
-    []
+    [
+      extra_applications: [:logger]
+    ]
   end
 
   defp description() do
@@ -31,7 +44,11 @@ defmodule ExChroma.MixProject do
     [
       files: ~w(lib .formatter.exs mix.exs README* CHANGELOG*),
       licenses: ["Apache-2.0"],
-      links: %{"GitHub" => "https://github.com/dkuku/ex_chroma"}
+      links: %{
+        "GitHub" => @source_url,
+        "Changelog" => "#{@source_url}/blob/master/CHANGELOG.md",
+      }
+
     ]
   end
 
@@ -39,8 +56,22 @@ defmodule ExChroma.MixProject do
     [
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.6", only: :test},
+      {:git_ops, "~> 2.4.2", only: [:dev]}
       #  {:propcheck, "~> 1.3", github: "alfert/propcheck", only: [:dev, :test]},
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      extras: [
+        "README.md",
+        "CHANGELOG.md"
+      ]
     ]
   end
 end
